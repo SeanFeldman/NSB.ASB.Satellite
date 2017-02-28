c8033b7f7337029b1d82322627d57c2840b6d9c3
Sean Feldman <feldman.sean@gmail.com>
2017-02-28 00:00:47 -0700

    Switching from locally referenced CRM assemblies to a nuget package

--- CRMAdapterEndpoint/CrmMessageHandler.cs
+++ CRMAdapterEndpoint/CrmMessageHandler.cs
@@ -4,19 +4,16 @@
     using NServiceBus;
     using NServiceBus.Logging;
     using System;
-   using CRMAdapterEndpoint.Messages;
+    using CRMAdapterEndpoint.Messages;
     using CustomerManagementMessages;
     using Microsoft.Xrm.Sdk;
-    using Microsoft.Xrm.Sdk.Client;
-
-
 
     public class NativeCrmMessageHandler : IHandleMessages<RemoteExecutionContext>
     {
         static ILog log = LogManager.GetLogger<NativeCrmMessageHandler>();
+
         public Task Handle(RemoteExecutionContext message, IMessageHandlerContext context)
         {
-           
             log.Info($"Received CRM message id: {context.MessageId} (body id: {message.CorrelationId})");
 
             //Messagename should be 'create'
@@ -26,7 +23,7 @@
             // NOTE: We could do some sort of adapter/command pattern using the entity name here...
             string entityname = message.PrimaryEntityName;
 
-            //Extract the contact information from the JSON based raw message and assign it into our message. 
+            //Extract the contact information from the JSON based raw message and assign it into our message.
             var NewCustomer = new NewCustomerReceived();
             NewCustomer.ContactId = message.PrimaryEntityId;
             NewCustomer.CreatedById = message.InitiatingUserId;
@@ -44,12 +41,11 @@
         private string GetCrmValue(RemoteExecutionContext message, string key)
         {
             var returnvalue = string.Empty;
-           // var attribute = message.InputParameters.Contains[key];//.ToString()//value.Attributes.Find(x => x.key == key);
+            // var attribute = message.InputParameters.Contains[key];//.ToString()//value.Attributes.Find(x => x.key == key);
 
             if (message.InputParameters.Contains(key))
             {
                 returnvalue = message.InputParameters[key].ToString();
-
             }
 
             return returnvalue;
@@ -62,7 +58,7 @@
 
         public Task Handle(CrmMessage message, IMessageHandlerContext context)
         {
-             log.Info($"Received CRM message id: {context.MessageId} (body id: {message.CorrelationId})");
+            log.Info($"Received CRM message id: {context.MessageId} (body id: {message.CorrelationId})");
 
             //Messagename should be 'create'
             string messagename = message.MessageName;
@@ -71,12 +67,12 @@
             // NOTE: We could do some sort of adapter/command pattern using the entity name here...
             string entityname = message.PrimaryEntityName;
 
-            //Extract the contact information from the JSON based raw message and assign it into our message. 
+            //Extract the contact information from the JSON based raw message and assign it into our message.
             var NewCustomer = new NewCustomerReceived();
             NewCustomer.ContactId = new Guid(message.PrimaryEntityId);
             NewCustomer.CreatedById = new Guid(message.InitiatingUserId);
             DateTime createddate = message.OperationCreatedOn;
-            NewCustomer.FullName = GetCrmValue(message,"fullname");
+            NewCustomer.FullName = GetCrmValue(message, "fullname");
             NewCustomer.FirstName = GetCrmValue(message, "firstname");
             NewCustomer.LastName = GetCrmValue(message, "lastname");
             NewCustomer.Address = GetCrmValue(message, "address1_composite");
@@ -84,7 +80,7 @@
 
             return context.Publish(NewCustomer);
 
-            
+
         }
 
         private string GetCrmValue(CrmMessage message, string key)
@@ -95,7 +91,6 @@
             if (attribute != null)
             {
                 returnvalue = message.InputParameters[0].value.Attributes.Find(x => x.key == key).value.ToString();
-
             }
 
             return returnvalue;
